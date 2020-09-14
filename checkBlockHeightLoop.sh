8f4a238f1f81e5dbecda52cce3ea506ddf2736f2
Chuck McKenna <chuckularone@gmail.com>
2020-09-14 13:41:09 -0400

    new stuff

--- checkBlockHeightLoop.sh
+++ checkBlockHeightLoop.sh
@@ -23,7 +23,12 @@ while true; do
     then
         minerHeight01=$(docker exec $MINER01 miner info height | awk '{print $2}')
         testVal01=$(expr $actual - $minerHeight01)   
-        echo "Miner01 Height:\033[1m $minerHeight01\033[0m    \033[41m$testVal01\033[m"
+        if [ $testVal01 -ge 1 ]
+        then
+            echo "Miner01 Height:\033[1m $minerHeight01\033[0m    \033[41m$testVal01\033[m"
+        else
+            echo "Miner01 Height:\033[1m $minerHeight01\033[0m    \033[33m$testVal01\033[m"
+        fi
         echo "MINER01, $datenow, $testVal01" >> log.out
     fi
 
@@ -31,7 +36,12 @@ while true; do
     then
         minerHeight02=$(docker exec $MINER02 miner info height | awk '{print $2}')
         testVal02=$(expr $actual - $minerHeight02)   
-        echo "Miner02 Height:\033[1m $minerHeight02\033[0m    \033[41m$testVal02\033[m"
+        if [ $testVal02 -ge 1 ] 
+        then
+            echo "Miner02 Height:\033[1m $minerHeight02\033[0m    \033[41m$testVal02\033[m"
+        else
+            echo "Miner02 Height:\033[1m $minerHeight02\033[0m    \033[33m$testVal02\033[m"
+        fi
         echo "MINER02, $datenow, $testVal02" >> log.out
     fi
 
@@ -39,7 +49,12 @@ while true; do
     then
         minerHeight03=$(docker exec $MINER03 miner info height | awk '{print $2}')
         testVal03=$(expr $actual - $minerHeight03)   
-        echo "Miner03 Height:\033[1m $minerHeight03\033[0m    \033[41m$testVal03\033[m"
+        if [ $testVal03 -ge 1 ] 
+        then
+            echo "Miner03 Height:\033[1m $minerHeight03\033[0m    \033[41m$testVal03\033[m"
+        else
+            echo "Miner03 Height:\033[1m $minerHeight03\033[0m    \033[33m$testVal03\033[m"
+        fi
         echo "MINER03, $datenow, $testVal02" >> log.out
     fi    
 
@@ -48,6 +63,6 @@ while true; do
     echo " "
     echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
     echo " "
-    docker ps
+    #docker ps
     sleep 30
 done
---
+++ minerUpdate02.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Script for auto updating the helium miner.
+
+# Set default values
+MINER=miner02
+REGION=US915
+GWPORT=1681
+MINERPORT=44159
+DATADIR=/home/chuck/miner/miner_data_02
+
+# Read switches to override any default values for non-standard configs
+while getopts n:g:p:d:r: flag
+do
+   case "${flag}" in
+      n) MINER=${OPTARG};;
+      g) GWPORT=${OPTARG};;
+      p) MINERPORT=${OPTARG};;
+      d) DATADIR=${OPTARG};;
+      r) REGION=${OPTARG};;
+   esac
+done
+
+# Autodetect running image version and set arch
+running_image=$(docker container inspect -f '{{.Config.Image}}' $MINER | awk -F: '{print $2}')
+if [ `echo $running_image | awk -F_ '{print $1}'` = "miner-arm64" ]; then
+   ARCH=arm
+else
+   ARCH=amd
+fi
+
+#miner_latest=$(curl -s 'https://quay.io/api/v1/repository/team-helium/miner/tag/?limit=100&page=1&onlyActiveTags=true' | jq -c --arg ARCH "$ARCH" '[ .tags[] | select( .name | contains($ARCH)) ][0].name' | cut -d'"' -f2)
+
+miner_quay=$(curl -s 'https://quay.io/api/v1/repository/team-helium/miner/tag/?limit=100&page=1&onlyActiveTags=true' --write-out '\nHTTP_Response:%{http_code}')
+
+miner_response=$(echo "$miner_quay" | grep "HTTP_Response" | cut -d":" -f2)
+
+if [[ $miner_response -ne 200 ]];
+	then
+	echo "Bad Response from Server"
+	exit 0
+fi
+
+miner_latest=$(echo "$miner_quay" | grep -v HTTP_Response | jq -c --arg ARCH "$ARCH" '[ .tags[] | select( .name | contains($ARCH)) ][0].name' | cut -d'"' -f2)
+
+date
+
+if `echo $miner_latest | grep -q $ARCH`;
+then echo "Latest miner version" $miner_latest;
+elif miner_latest=$(curl -s 'https://quay.io/api/v1/repository/team-helium/miner/tag/?limit=100&page=1&onlyActiveTags=true' | jq -r .tags[1].name)
+then echo "Latest miner version" $miner_latest;
+fi
+
+
+if [ $miner_latest = $running_image ];
+then    echo "already on the latest version"
+        exit 0
+fi
+
+echo "Stopping and removing old miner"
+
+docker stop $MINER && docker rm $MINER
+
+echo "Provisioning new miner version"
+
+docker run -d --env REGION_OVERRIDE=$REGION --restart always --publish $GWPORT:$GWPORT/udp --publish $MINERPORT:$MINERPORT/tcp --name $MINER --mount type=bind,source=$DATADIR,target=/var/data quay.io/team-helium/miner:$miner_latest
+
+if [ $GWPORT -ne 1680 ] || [ $MINERPORT -ne 44158 ]; then
+   echo "Using nonstandard ports, adjusting miner config"
+   docker exec $MINER sed -i "s/44158/$MINERPORT/; s/1680/$GWPORT/" /opt/miner/releases/0.1.0/sys.config
+   docker restart $MINER
+fi
