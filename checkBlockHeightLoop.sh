222de5e50df519c7f6bb2f8db9d785ebfd1a1aa6
Chuck McKenna <chuck@mckenna.pub>
2020-09-14 10:10:12 -0400

    Update checkBlockHeightLoop.sh
    Moved some code

--- checkBlockHeightLoop.sh
+++ checkBlockHeightLoop.sh
@@ -40,16 +40,19 @@ echo " "
  
 if [ $varLen01 -gt 0 ]
 then
+    testVal01=$(expr $actual - $minerHeight01)   
     echo "Miner01 Height:\033[1m $minerHeight01\033[0m    \033[41m$testVal01\033[m"
 fi
 
 if [ $varLen02 -gt 0 ]
 then
+    testVal02=$(expr $actual - $minerHeight02)   
     echo "Miner02 Height:\033[1m $minerHeight02\033[0m    \033[41m$testVal02\033[m"
 fi
 
 if [ $varLen03 -gt 0 ]
 then
+    testVal03=$(expr $actual - $minerHeight03)   
     echo "Miner03 Height:\033[1m $minerHeight03\033[0m    \033[41m$testVal03\033[m"
 fi    
 echo " "
@@ -72,7 +75,6 @@ fi
 
 if [ $varLen03 -gt 0 ]
 then
-    testVal03=$(expr $actual - $minerHeight03)   
     echo "MINER03, $datenow, $testVal02" >> log.out
 fi
 
